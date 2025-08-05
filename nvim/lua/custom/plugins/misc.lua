86c87bb846d269a3ff3e763d69beab349eb6b82f
Ignacio Acosta <ignacioa@soyio.id>
2025-08-04 23:08:55 -0400

    feat(nvim): add todo comments plugin

--- nvim/lua/custom/plugins/misc.lua
+++ nvim/lua/custom/plugins/misc.lua
@@ -3,4 +3,12 @@ return {
     'NMAC427/guess-indent.nvim',
     opts = {},
   },
+  {
+    'folke/todo-comments.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('todo-comments').setup { signs = false }
+      vim.keymap.set('n', '<leader>st', ':TodoTelescope<CR>', { desc = '[T]odo [T]elescope' })
+    end,
+  },
 }
