212e3ce8184c0115bbdcd0074b2394ef2be72207
Ignacio Acosta <ignacioa@soyio.id>
2026-01-25 22:11:29 -0300

    feat(neovim): add keybind for git blame

--- nvim/lua/custom/plugins/git.lua
+++ nvim/lua/custom/plugins/git.lua
@@ -3,6 +3,7 @@ return {
     'tpope/vim-fugitive',
     config = function()
       vim.keymap.set('n', '<leader>gs', ':Git<CR>', { noremap = true, silent = true, desc = '[G]it [S]tatus' })
+      vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', { noremap = true, silent = true, desc = '[G]it [B]lame' })
     end,
   },
   {
