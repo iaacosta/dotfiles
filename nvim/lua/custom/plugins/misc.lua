b2eb4e00c01968af3b07814c78a1fcaeb3d7fd47
Ignacio Acosta <iaacosta@uc.cl>
2026-01-27 00:59:01 -0300

    fix(nvim): remove the codesnap plugin

--- nvim/lua/custom/plugins/misc.lua
+++ nvim/lua/custom/plugins/misc.lua
@@ -11,20 +11,4 @@ return {
       vim.keymap.set('n', '<leader>st', ':TodoTelescope<CR>', { desc = '[T]odo [T]elescope' })
     end,
   },
-  {
-    'mistricky/codesnap.nvim',
-    build = 'make',
-    keys = {
-      { '<leader>cs', '<cmd>CodeSnap<cr>', mode = 'x', desc = '[C]ode[S]nap' },
-    },
-    config = function()
-      require('codesnap').setup {
-        has_line_number = true,
-        watermark = '',
-        bg_padding = 16,
-        mac_window_bar = false,
-      }
-    end,
-  },
-
 }
