82581e29e070c5ed7fef68006f877db6cd453d7e
Ignacio Acosta <iaacosta@uc.cl>
2026-02-25 13:35:33 -0300

    feat(nvim): add gbrowse compat

--- nvim/lua/custom/plugins/git.lua
+++ nvim/lua/custom/plugins/git.lua
@@ -1,6 +1,9 @@
 return {
   {
     'tpope/vim-fugitive',
+    dependencies = {
+      'tpope/vim-rhubarb'
+    },
     config = function()
       vim.keymap.set('n', '<leader>gs', ':Git<CR>', { noremap = true, silent = true, desc = '[G]it [S]tatus' })
       vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', { noremap = true, silent = true, desc = '[G]it [B]lame' })
