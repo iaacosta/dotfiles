b5833ed683ef440931902a1ae6d488e9c39c3045
Ignacio Acosta <iaacosta@uc.cl>
2026-02-25 13:35:09 -0300

    fix(nvim): bad keybinding name

--- nvim/lua/custom/plugins/misc.lua
+++ nvim/lua/custom/plugins/misc.lua
@@ -8,7 +8,7 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       require('todo-comments').setup { signs = false }
-      vim.keymap.set('n', '<leader>st', ':TodoTelescope<CR>', { desc = '[T]odo [T]elescope' })
+      vim.keymap.set('n', '<leader>st', ':TodoTelescope<CR>', { desc = '[S]earch [T]odo' })
     end,
   },
 }
